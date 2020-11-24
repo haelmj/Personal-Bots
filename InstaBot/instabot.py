da2ab0e27dc95b94daa2d072e071f408768b4ed4
Michael Ndon <manj19.mjn@gmail.com>
2020-11-24 18:03:22 +0100

    Initialized Bot class

--- InstaBot/instabot.py
+++ InstaBot/instabot.py
@@ -1,4 +1,15 @@
 from instapy import InstaPy
 from instapy import smart_run
+import os
 
+INSTA_USER = os.environ.get('INSTA_USER')
+INSTA_PASS = os.environ.get('INSTA_PASS')
 
+class IBot():
+    def __init__(self):
+        self.session = InstaPy(username=INSTA_USER, password=INSTA_PASS)
+    
+    def startsession(self):
+        with smart_run(self.session):
+            """Activity Flow"""
+            self.session.like_by_tags(['python3', 'javascript'], amount=40)
